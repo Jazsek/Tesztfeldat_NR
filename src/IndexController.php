5742235f9d367c230924c6496fecf90c644bcbc7
Jaszek <jaszek94@gmail.com>
2023-02-18 10:22:54 +0100

    Update

--- src/IndexController.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-class IndexController {
-    public function searchInMultidimensionalArray(array $array, $search_value, $search_field){
-        return array_search($search_value, array_column($array, $search_field));
-    }
-}
